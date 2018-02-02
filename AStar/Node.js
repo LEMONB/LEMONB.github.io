c6eb8cea4239cb9c90c7a11a131e333cb72e3128
LEMONB <quietjbs@gmail.com>
2018-02-02 23:24:41 +0300

    1

--- AStar/Node.js
+++ AStar/Node.js
@@ -1,93 +1,83 @@
+function Node( i, j ) {
+	this.i = i;
+	this.j = j;
+	this.f = 0;
+	this.g = 0;
+	this.h = 0;
+	this.isWall = false;
+	this.isClosed = false;
+	this.isOpened = false;
+	this.previous = null;
+	this.neighbors = [];
 
+	this.generateWall = function( wallsProbability ) {
+		if ( this != startNode && this != targetNode ) {
+			if ( random( 1 ) < wallsProbability )
+				this.isWall = true;
+			else
+				this.isWall = false;
+		}
+	}
 
-function Node(i, j) {
-  this.i = i;
-  this.j = j;
-  this.f = 0;
-  this.g = 0;
-  this.h = 0;
-  this.isWall = false;
-  this.previous = null;
-  this.neighbors = [];
+	this.show = function() {
+		if ( this.isWall == true ) {
+			fill( 0 );
+			rect( this.i * wid + wid / 6, this.j * wid + wid / 6, wid - 1 - wid / 6, wid - 1 - wid / 6 );
+			return;
+		} else if ( this == startNode )
+			fill( 0, 255, 255 );
+		else if ( this == targetNode )
+			fill( 255, 0, 255 );
+		else if ( this.isOpened )
+			fill( 0, 255, 0, 150 );
+		else if ( this.isClosed )
+			fill( 255, 0, 0, 150 );
+		else
+			return;
 
-  this.generateWall = function(wallsProbability) {
-    if (this != startNode && this != targetNode) {
-      if (random(1) < wallsProbability)
-        this.isWall = true;
-      else
-        this.isWall = false;
-    }
-  }
+		ellipse( this.i * wid + wid / 2, this.j * wid + wid / 2, wid / 2, wid / 2 );
+	}
 
-  this.show = function(r, g, b) {
-    if ( this.isWall == true) {
-      fill(0, 0, 0);
-      rect( this.i * wid + wid/6, this.j * wid + wid/6, wid-1 - wid/6, wid-1 - wid/6);
-      return;
-    } else if (this == startNode)
-      fill(0, 255, 255);
-    else if (this == targetNode)
-      fill(255, 0, 255);
-    else if (r == 0 && g == 255 && b == 0)
-      fill(r, g, b, 150);
-    else if (r == 255 && g == 0 && b == 0)
-      fill(r, g, b, 150);
-    else
-      return;
-    //fill(r, g, b);
+	this.pushNeighbors = function( _nodes ) {
+		this.neighbors = [];
 
-    ellipse(this.i * wid + wid/2, this.j * wid + wid/2, wid/2, wid/2);
-    //rect( this.i * wid,  this.j * wid, wid-1, wid-1);
-  }
+		//LRUD
+		if ( this.i < arrSize - 1 && _nodes[ this.i + 1 ][ this.j ].isWall == false ) {
+			this.neighbors.push( _nodes[ this.i + 1 ][ this.j ] );
+		}
+		if ( this.i > 0 && _nodes[ this.i - 1 ][ this.j ].isWall == false ) {
+			this.neighbors.push( _nodes[ this.i - 1 ][ this.j ] );
+		}
+		if ( this.j < arrSize - 1 && _nodes[ this.i ][ this.j + 1 ].isWall == false ) {
+			this.neighbors.push( _nodes[ this.i ][ this.j + 1 ] );
+		}
+		if ( this.j > 0 && _nodes[ this.i ][ this.j - 1 ].isWall == false ) {
+			this.neighbors.push( _nodes[ this.i ][ this.j - 1 ] );
+		}
 
-  this.pushNeighbors = function(_nodes) {
-    this.neighbors = [];
+		//DIAG
+		if ( this.i < arrSize - 1 ) {
+			if ( this.j < arrSize - 1 && _nodes[ this.i + 1 ][ this.j ].isWall == false &&
+				_nodes[ this.i ][ this.j + 1 ].isWall == false ) {
+				this.neighbors.push( _nodes[ this.i + 1 ][ this.j + 1 ] );
+			}
 
-    //LRUD
-    if ( this.i < arrSize - 1 && _nodes[ this.i + 1][ this.j].isWall == false) {
-      this.neighbors.push(_nodes[ this.i + 1][ this.j]);
-    }
-    if ( this.i > 0 && _nodes[ this.i - 1][ this.j].isWall == false) {
-      this.neighbors.push(_nodes[ this.i - 1][ this.j]);
-    }
-    if ( this.j < arrSize - 1 && _nodes[ this.i][ this.j + 1].isWall == false) {
-      this.neighbors.push(_nodes[ this.i][ this.j + 1]);
-    }
-    if ( this.j > 0 && _nodes[ this.i][ this.j - 1].isWall == false) {
-      this.neighbors.push(_nodes[ this.i][ this.j - 1]);
-    }
+			if ( this.j > 0 && _nodes[ this.i + 1 ][ this.j ].isWall == false && _nodes[ this
+					.i ][ this.j - 1 ].isWall == false ) {
+				this.neighbors.push( _nodes[ this.i + 1 ][ this.j - 1 ] );
+			}
+		}
 
-    //DIAG
-    if ( this.i < arrSize - 1) {
-      if ( this.j < arrSize - 1
-        && _nodes[this.i + 1][this.j].isWall == false
-        && _nodes[this.i][this.j + 1].isWall == false)
-      {
-        this.neighbors.push(_nodes[ this.i + 1][ this.j + 1]);
-      }
+		if ( this.i > 0 ) {
+			if ( this.j < arrSize - 1 && _nodes[ this.i - 1 ][ this.j ].isWall == false &&
+				_nodes[ this.i ][ this.j + 1 ].isWall == false ) {
+				this.neighbors.push( _nodes[ this.i - 1 ][ this.j + 1 ] );
+			}
 
-      if (this.j > 0
-        && _nodes[this.i + 1][this.j].isWall == false
-        && _nodes[this.i][this.j - 1].isWall == false)
-      {
-        this.neighbors.push(_nodes[ this.i + 1][ this.j - 1]);
-      }
-    }
-
-    if ( this.i > 0) {
-      if ( this.j < arrSize - 1
-        && _nodes[this.i - 1][this.j].isWall == false
-        && _nodes[this.i][this.j + 1].isWall == false)
-      {
-        this.neighbors.push(_nodes[ this.i - 1][ this.j + 1]);
-      }
-
-      if (this.j > 0
-        && _nodes[this.i - 1][this.j].isWall == false
-        && _nodes[this.i][this.j - 1].isWall == false)
-      {
-        this.neighbors.push(_nodes[ this.i - 1][ this.j - 1]);
-      }
-    }
-  }
+			if ( this.j > 0 && _nodes[ this.i - 1 ][ this.j ].isWall == false && _nodes[ this
+					.i ][ this.j - 1 ].isWall == false ) {
+				this.neighbors.push( _nodes[ this.i - 1 ][ this.j - 1 ] );
+			}
+		}
+	}
 }
--- AStar/newRes.js
+++ AStar/newRes.js
@@ -1,31 +1,31 @@
-function removeFromArray(arr, obj) {
-  for (var i = arr.length - 1; i >= 0; i--) {
-    if (arr[i] == obj) {
-      arr.splice(i, 1);
-    }
-  }
+function removeFromArray( arr, obj ) {
+	for ( var i = arr.length - 1; i >= 0; i-- ) {
+		if ( arr[ i ] == obj ) {
+			arr.splice( i, 1 );
+		}
+	}
 }
 
-function Heuristic(from, target) {
-  //var d = abs(from.i - target.i) + abs(from.j - target.j);
-  var d = dist(from.i, from.j, target.i, target.j);
-  return d;
+function Heuristic( from, target ) {
+	//var d = abs(from.i - target.i) + abs(from.j - target.j);
+	var d = dist( from.i, from.j, target.i, target.j );
+	return d;
 }
 
 var arrSize = 25;
 var wid = 0;
 
-var nodes =  new Array(arrSize);
-var openSet =  [];
-var closedSet =  [];
-var path =  [];
+var nodes = new Array( arrSize );
+var openSet = [];
+// var closedSet = [];
+var path = [];
 var startNode = null;
 var targetNode = null;
-//var current = null;
+var current = null;
 
-var isReady =  false;
-var isBuilding =  false;
-var isSolved = false;
+var isReady = false;
+var isBuilding = false;
+var isNoLoop = false;
 
 var isControlsExist = false;
 var genWallsButton;
@@ -34,264 +34,262 @@ var resetButton;
 var wallDens;
 var gridSize;
 var gridSizeButton;
+
 document.oncontextmenu = function() {
-  return false;
-}
-
-function setup() {
-  createCanvas(800, 800);
-  console.log("START");
-  background(51);
-  wid = height / arrSize;
-
-  if (isControlsExist == false) {
-    startButton = createButton("Start");
-    startButton.position(500, 15);
-    startButton.mousePressed(startSearch);
-
-    resetButton = createButton("Reset");
-    resetButton.position(startButton.x + startButton.width + 10, startButton.y);
-    resetButton.mousePressed(reset);
-
-    wallDens = createSlider(0, 100, 25);
-    wallDens.position(startButton.x, startButton.y + startButton.height + 10);
-
-    genWallsButton = createButton("Generate Walls");
-    genWallsButton.position(wallDens.x + wallDens.width + 20, wallDens.y);
-    genWallsButton.mousePressed(generateWalls);
-
-
-    gridSize = createSlider(5, 100, 25);
-    gridSize.position(wallDens.x, wallDens.y + wallDens.height + 10);
-
-    gridSizeButton = createButton("Set Size");
-    gridSizeButton.position(gridSize.x + gridSize.width + 20, gridSize.y);
-    gridSizeButton.mousePressed(setSizeOfGrid);
-
-    isControlsExist = true;
-  }
-
-  //Creating 2D array
-  for (var i =  0; i < arrSize; i++) {
-    nodes[i] = new Array(arrSize);
-  }
-
-  for (var i =  0; i < arrSize; i++) {
-    for (var j =  0; j < arrSize; j++) {
-      nodes[i][j] = new Node(i, j);
-    }
-  }
-  for (var i =  0; i < arrSize; i++) {
-    for (var j =  0; j < arrSize; j++) {
-      nodes[i][j].pushNeighbors(nodes);
-    }
-  }
-
-  //startNode = nodes[int(random(arrSize-1))][int(random(arrSize-1))];
-  //targetNode = nodes[int(random(arrSize-1))][int(random(arrSize-1))];
-  startNode = nodes[0][0];
-  targetNode = nodes[arrSize-1][arrSize-1];
-  startNode.isWall = false;
-  targetNode.isWall = false;
-  openSet.push(startNode);
-
-  for (var i =  0; i < arrSize; i++) {
-    for (var j =  0; j< arrSize; j++) {
-      nodes[i][j].show(255, 255, 255);
-    }
-  }
-  loop();
+	return false;
 }
 
 function setSizeOfGrid() {
-  arrSize = gridSize.value();
-  wid = height / arrSize;
-  reset();
+	arrSize = gridSize.value();
+	wid = height / arrSize;
+	reset();
 }
 
 function generateWalls() {
-  var wallsProbability = wallDens.value() / 100;
+	var wallsProbability = wallDens.value() / 100;
 
-  //console.log(wallDens.value() + "%");
-  for (var i =  0; i < arrSize; i++) {
-    for (var j =  0; j< arrSize; j++) {
-      nodes[i][j].generateWall(wallsProbability);
-    }
-  }
+	//console.log(wallDens.value() + "%");
+	for ( var i = 0; i < arrSize; i++ ) {
+		for ( var j = 0; j < arrSize; j++ ) {
+			nodes[ i ][ j ].generateWall( wallsProbability );
+		}
+	}
 }
 
 function startSearch() {
-  isReady = true;
+	isReady = true;
 }
 
 function reset() {
-  nodes =  new Array(arrSize);
-  openSet = [];
-  closedSet = [];
-  path = [];
-  startNode = null;
-  targetNode = null;
-  //current = null;
+	nodes = new Array( arrSize );
+	openSet = [];
+	// closedSet = [];
+	path = [];
+	startNode = null;
+	targetNode = null;
+	current = null;
 
-  isReady =  false;
-  isBuilding =  false;
-  isSolved = false;
-  setup();
+	isReady = false;
+	isBuilding = false;
+	isNoLoop = false;
+	setup();
 }
 
 function mouseReleased() {
-  if (isReady === true)
-    return;
-  isBuilding = false;
+	if ( isReady === true )
+		return;
+	isBuilding = false;
 }
 
 function keyPressed() {
-  if (keyCode === 82) {
-    reset();
-  }
-  //console.log(isReady);
+	if ( keyCode === 82 ) {
+		reset();
+	}
+	//console.log(isReady);
 
-  if (isReady === true) {
-    return;
-  }
+	if ( isReady === true ) {
+		return;
+	}
 
-  if (keyCode === 66) {
-    isReady = true;
-  }
+	if ( keyCode === 66 ) {
+		isReady = true;
+	}
 
-  if (keyCode === 71) {
-    generateWalls();
-  }
-  for (var i =  0; i < arrSize; i++) {
-    for (var j =  0; j< arrSize; j++) {
-      nodes[i][j].show(255, 255, 255);
-    }
-  }
+	if ( keyCode === 71 ) {
+		generateWalls();
+	}
+	for ( var i = 0; i < arrSize; i++ ) {
+		for ( var j = 0; j < arrSize; j++ ) {
+			nodes[ i ][ j ].show();
+		}
+	}
+}
+
+function setup() {
+	createCanvas( 750, 750 );
+	console.log( "START" );
+	wid = height / arrSize;
+
+	if ( isControlsExist == false ) {
+		startButton = createButton( "Start" );
+		startButton.position( 500, 15 );
+		startButton.mousePressed( startSearch );
+
+		resetButton = createButton( "Reset" );
+		resetButton.position( startButton.x + startButton.width + 10, startButton.y );
+		resetButton.mousePressed( reset );
+
+		wallDens = createSlider( 0, 100, 25 );
+		wallDens.position( startButton.x, startButton.y + startButton.height + 10 );
+
+		genWallsButton = createButton( "Generate Walls" );
+		genWallsButton.position( wallDens.x + wallDens.width + 20, wallDens.y );
+		genWallsButton.mousePressed( generateWalls );
+
+
+		gridSize = createSlider( 5, 100, 25 );
+		gridSize.position( wallDens.x, wallDens.y + wallDens.height + 10 );
+
+		gridSizeButton = createButton( "Set Size" );
+		gridSizeButton.position( gridSize.x + gridSize.width + 20, gridSize.y );
+		gridSizeButton.mousePressed( setSizeOfGrid );
+
+		isControlsExist = true;
+	}
+
+	//Creating 2D array
+	for ( var i = 0; i < arrSize; i++ ) {
+		nodes[ i ] = new Array( arrSize );
+	}
+
+	for ( var i = 0; i < arrSize; i++ ) {
+		for ( var j = 0; j < arrSize; j++ ) {
+			nodes[ i ][ j ] = new Node( i, j );
+		}
+	}
+	for ( var i = 0; i < arrSize; i++ ) {
+		for ( var j = 0; j < arrSize; j++ ) {
+			nodes[ i ][ j ].pushNeighbors( nodes );
+		}
+	}
+
+	//startNode = nodes[int(random(arrSize-1))][int(random(arrSize-1))];
+	//targetNode = nodes[int(random(arrSize-1))][int(random(arrSize-1))];
+	startNode = nodes[ 0 ][ 0 ];
+	targetNode = nodes[ arrSize - 1 ][ arrSize - 1 ];
+	startNode.isWall = false;
+	targetNode.isWall = false;
+	openSet.push( startNode );
+	startNode.isOpened = true;
+	loop();
 }
 
 function draw() {
-  if (isSolved == false) {
-    background(220);
-    stroke(0);
-    strokeWeight(1);
-    for (var i =  0; i < arrSize; i++) {
-      for (var j =  0; j< arrSize; j++) {
-        nodes[i][j].show(255, 255, 255);
-      }
-    }
-  }
+	// console.log(openSet.length + " " + closedSet.length);
+	if ( isNoLoop == true ) {
+		return;
+	}
 
-  //MOUSE CONTROLS FOR BUILDING WALLS
-  if (isReady == false) {
-    //console.log("BUILDING WALLS");
-    if (mouseIsPressed) {
-      if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-        //console.log("CLICKED " + int(mouseX / wid) + " " + int(mouseY / wid));
-        if (mouseButton === LEFT) {
-          nodes[int(mouseX / wid)][int(mouseY / wid)].isWall = true;
-        } else if (mouseButton ===  RIGHT) {
-          nodes[int(mouseX / wid)][int(mouseY / wid)].isWall = false;
-        }
-      }
-    }
+	background( 220 );
+	for ( var i = 0; i < arrSize; i++ ) {
+		for ( var j = 0; j < arrSize; j++ ) {
+			nodes[ i ][ j ].show();
+		}
+	}
 
-    for (var i =  0; i < arrSize; i++) {
-      for (var j =  0; j < arrSize; j++) {
-        nodes[i][j].pushNeighbors(nodes);
-      }
-    }
-  }
+	//MOUSE CONTROLS FOR BUILDING WALLS
+	if ( isReady == false ) {
+		//console.log("BUILDING WALLS");
+		if ( mouseIsPressed ) {
+			if ( mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height ) {
+				//console.log("CLICKED " + int(mouseX / wid) + " " + int(mouseY / wid));
+				if ( mouseButton === LEFT ) {
+					nodes[ int( mouseX / wid ) ][ int( mouseY / wid ) ].isWall = true;
+				} else if ( mouseButton ===  RIGHT ) {
+					nodes[ int( mouseX / wid ) ][ int( mouseY / wid ) ].isWall = false;
+				}
+			}
+			for ( var i = 0; i < arrSize; i++ ) {
+				for ( var j = 0; j < arrSize; j++ ) {
+					nodes[ i ][ j ].pushNeighbors( nodes );
+				}
+			}
+		}
 
-  if (isReady == false)
-    return;
-  //console.log("SEARCHING FOR TARGET");
-  //console.log(openSet.length + " " + closedSet.length);
+	}
 
-  if (isSolved == true)
-    return;
+	if ( isReady == false )
+		return;
+	//console.log("SEARCHING FOR TARGET");
 
-  //ASTAR ALGORITHM
-  if (openSet.length > 0) {
-    var winnerInd =  0;
-    for (var i = 0; i < openSet.length; i++) {
-      if (openSet[i].f < openSet[winnerInd].f) {
-        winnerInd = i;
-        //console.log(i);
-      }
-    }
+	//ASTAR ALGORITHM
+	if ( openSet.length > 0 ) {
+		var winnerInd = 0;
+		for ( var i = 0; i < openSet.length; i++ ) {
+			if ( openSet[ i ].f < openSet[ winnerInd ].f ) {
+				winnerInd = i;
+				//console.log(i);
+			}
+		}
 
-    var current = openSet[winnerInd];
+		current = openSet[ winnerInd ];
 
-    if (current == targetNode) {
-      console.log("DONE!");
-      isSolved = true;
-      noLoop();
-      //return;
-    }
+		if ( current == targetNode ) {
+			console.log( "DONE!" );
+			isNoLoop = true;
 
-    removeFromArray(openSet, current);
-    closedSet.push(current);
+			reconstructPath( current );
+			noLoop();
+			// return;
+		}
 
-    for (var i =  0; i < current.neighbors.length; i++) {
-      if (!closedSet.includes(current.neighbors[i]) && current.neighbors[i].isWall == false) {
+		removeFromArray( openSet, current );
+		// closedSet.push( current );
+		current.isOpened = false;
+		current.isClosed = true;
 
-        var tempG =  current.g + Heuristic(current.neighbors[i], current);
 
-        // Is this a better path than before?
-        var newPath = false;
-        if (openSet.includes(current.neighbors[i])) {
-          if (tempG < current.neighbors[i].g) {
-            current.neighbors[i].g = tempG;
-            newPath = true;
-          }
-        } else {
-          current.neighbors[i].g = tempG;
-          newPath = true;
-          openSet.push(current.neighbors[i]);
-        }
+		for ( var i = 0; i < current.neighbors.length; i++ ) {
+			var neighbor = current.neighbors[ i ];
+			// if ( !closedSet.includes( neighbor ) && neighbor.isWall == false ) {
+			if ( neighbor.isClosed == false && neighbor.isWall == false ) {
 
-        // Yes, it's a better path
-        if (newPath) {
-          current.neighbors[i].h = Heuristic(current.neighbors[i], targetNode);
-          current.neighbors[i].f = current.neighbors[i].g + current.neighbors[i].h;
-          current.neighbors[i].previous = current;
-        }
-      }
-    }
-  } else {
-     console.log("NO SOLUTION");
-    isSolved = true;
-     noLoop();
-    return;
-  }
-  for (var i =  0; i < closedSet.length; i++) {
-    closedSet[i].show(255, 0, 0);
-  }
+				var tempG = current.g + Heuristic( neighbor, current );
 
-  for (var i =  0; i < openSet.length; i++) {
-    openSet[i].show(0, 255, 0);
-  }
+				// Is this a better path than before?
+				var newPath = false;
+				// if ( openSet.includes( neighbor ) ) {
+				if ( neighbor.isOpened ) {
+					if ( tempG < neighbor.g ) {
+						neighbor.g = tempG;
+						newPath = true;
+					}
+				} else {
+					neighbor.g = tempG;
+					newPath = true;
+					openSet.push( neighbor );
+					neighbor.isOpened = true;
+				}
 
-  path = [];
-  var temp =  current;
-  path.push(temp);
-  while (temp.previous != null) {
-    path.push(temp.previous);
-    temp = temp.previous;
-  }
+				// Yes, it's a better path
+				if ( newPath ) {
+					neighbor.h = Heuristic( neighbor, targetNode );
+					neighbor.f = neighbor.g + neighbor.h;
+					neighbor.previous = current;
+				}
+			}
+		}
+	} else {
+		console.log( "NO SOLUTION" );
+		isNoLoop = true;
+		noLoop();
+		// return;
+	}
+	reconstructPath( current );
 
-  noFill();
-  stroke(0,0,255);
-  strokeWeight(4);
-  beginShape();
-  for (var i =  0; i < path.length; i++) {
-    vertex(path[i].i * wid + wid/2, path[i].j * wid + wid/2);
-  }
-  endShape();
+	//for (var i =  0; i < path.length; i++) {
+	//  path[i].show(0, 0, 255);
+	//}
+	//console.log(current);
+	//console.log(path);
+}
 
-  //for (var i =  0; i < path.length; i++) {
-  //  path[i].show(0, 0, 255);
-  //}
-  //console.log(current);
-  //console.log(path);
+function reconstructPath( from ) {
+	path = [];
+	var temp = from;
+	path.push( temp );
+	while ( temp.previous != null ) {
+		path.push( temp.previous );
+		temp = temp.previous;
+	}
+
+	noFill();
+	stroke( 0, 0, 255 );
+	strokeWeight( 4 );
+	beginShape();
+	for ( var i = 0; i < path.length; i++ ) {
+		vertex( path[ i ].i * wid + wid / 2, path[ i ].j * wid + wid / 2 );
+	}
+	endShape();
+	noStroke();
 }
